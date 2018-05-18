97ddc93379257a9f362ba8d2e0397597816752e7
James Forcier <csssuf@csssuf.net>
2018-05-17 22:29:59 -0700

    mediacontainer: add structures for Directories

--- src/mediacontainer.rs
+++ src/mediacontainer.rs
@@ -1,5 +1,20 @@
 use util;
 
+#[derive(Debug, Clone, Copy)]
+#[derive(Deserialize)]
+pub enum DirectoryType {
+    Unknown,
+    #[serde(rename = "movie")] Movies,
+    #[serde(rename = "artist")] Music,
+    #[serde(rename = "show")] Shows,
+}
+
+impl Default for DirectoryType {
+    fn default() -> DirectoryType {
+        DirectoryType::Unknown
+    }
+}
+
 #[derive(Debug, Clone, Default)]
 #[derive(Deserialize)]
 #[serde(default, rename_all = "camelCase")]
@@ -10,6 +25,7 @@ pub(crate) struct MediaContainer {
     // Optional fields. Only one of these will generally be present in a MediaContainer, so it's up
     // to the code creating this MediaContainer to know which to expect.
     #[serde(default, rename = "Server")] pub(crate) servers: Vec<MediaServerEntry>,
+    #[serde(default, rename = "Directory")] pub(crate) directories: Vec<DirectoryEntry>,
 }
 
 #[derive(Debug, Clone, Default)]
@@ -33,4 +49,25 @@ pub(crate) struct MediaServerEntry {
     #[serde(default)] pub(crate) source_title: String,
     #[serde(default)] pub(crate) owner_id: u64,
     #[serde(deserialize_with = "util::deserialize_xml_bool", default)] pub(crate) home: bool,
+}
+
+#[derive(Debug, Clone, Default)]
+#[derive(Deserialize)]
+#[serde(default, rename_all = "camelCase")]
+pub struct DirectoryEntry {
+    pub key: String,
+    pub title: String,
+    pub name: String,
+    #[serde(deserialize_with = "util::deserialize_xml_bool")] pub secondary: bool,
+    pub prompt: String,
+
+    // Library section related fields
+    #[serde(deserialize_with = "util::deserialize_xml_bool")] pub allow_sync: bool,
+    pub art: String,
+    pub composite: String,
+    #[serde(deserialize_with = "util::deserialize_xml_bool")] pub filters: bool,
+    #[serde(deserialize_with = "util::deserialize_xml_bool")] pub refreshing: bool,
+    pub thumb: String,
+    #[serde(rename = "type")] pub dir_type: DirectoryType,
+    pub agent: String,
 }
