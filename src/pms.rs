cf07f5679345d35404b8627dde4e8bfc94017366
James Forcier <csssuf@csssuf.net>
2018-05-17 22:30:18 -0700

    pms: add system() and library_sections() calls to PMS

--- src/pms.rs
+++ src/pms.rs
@@ -1,7 +1,9 @@
-use mediacontainer::MediaServerEntry;
+use mediacontainer::*;
 
+use failure::Error;
 use reqwest::Client;
 use reqwest::header::Headers;
+use serde_xml_rs;
 
 #[derive(Debug, Clone)]
 pub struct PlexMediaServer {
@@ -21,5 +23,43 @@ impl PlexMediaServer {
             headers: headers,
             entry: server,
         }
+    }
+
+    pub fn system(&self) -> Result<Vec<DirectoryEntry>, Error> {
+        let res = self.client
+            .get(&format!(
+                "{}://{}:{}/system",
+                self.entry.scheme,
+                self.entry.address,
+                self.entry.port
+            ))?
+            .headers(self.headers.clone())
+            .send()?;
+
+        let res_struct: MediaContainer = match serde_xml_rs::from_reader(res) {
+            Ok(s) => s,
+            Err(e) => bail!("deserialization error: {}", e),
+        };
+
+        Ok(Vec::from(res_struct.directories))
+    }
+
+    pub fn library_sections(&self) -> Result<Vec<DirectoryEntry>, Error> {
+        let res = self.client
+            .get(&format!(
+                "{}://{}:{}/library/sections",
+                self.entry.scheme,
+                self.entry.address,
+                self.entry.port
+            ))?
+            .headers(self.headers.clone())
+            .send()?;
+
+        let res_struct: MediaContainer = match serde_xml_rs::from_reader(res) {
+            Ok(s) => s,
+            Err(e) => bail!("deserialization error: {}", e),
+        };
+
+        Ok(Vec::from(res_struct.directories))
     }
 }
